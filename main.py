c80283a29692a830b5adf7699503a876819f8da9
ashley <ashlelijin7@gmail.com>
2023-06-24 23:26:18 +0530

    created (templates)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello world'
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
