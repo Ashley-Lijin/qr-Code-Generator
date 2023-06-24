60ec8397a1e10de16e76e04a4593b2a3593ea497
ashley <ashlelijin7@gmail.com>
2023-06-25 00:42:50 +0530

    added(qr code)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from io import BytesIO
+import qrcode
+from base64 import b64encode
 
 app = Flask(__name__)
 
@@ -8,5 +11,18 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/', methods=['POST'])
+def generateQR():
+    memory = BytesIO()
+    link = request.form.get('link')
+    qr = qrcode.make(link)
+    qr.save(memory)
+    memory.seek(0)
+
+    base64_img = "data:image/png;base64," + \
+        b64encode(memory.getvalue()).decode('ascii')
+
+    return render_template('index.html', data=base64_img)
+		
 if __name__ == "__main__":
     app.run(debug=True)
